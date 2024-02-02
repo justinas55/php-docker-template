0ada70f178529958fdba65d7bff762453fe71456
Justinas S <jsaldukas@gmail.com>
2024-02-02 11:22:23 +0200

    Update shell.sh - support running commands instead of opening a shell

--- shell.sh
+++ shell.sh
@@ -2,4 +2,10 @@
 
 set -e -o pipefail
 
-docker-compose exec app-web su -l app
+if [ -n "$1" ]; then
+    ESCAPED_ARGS=$(printf "%q " "$@")
+    echo "$ESCAPED_ARGS"
+    docker-compose exec app-web su -l app -c "$ESCAPED_ARGS"
+else
+    docker-compose exec app-web su -l app
+fi

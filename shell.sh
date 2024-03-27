5b495a9081acc495a437ad867d55453e58dbbc31
Justinas S <jsaldukas@gmail.com>
2024-03-27 18:03:55 +0200

    cleaner way to open shell with shell service in compose

--- shell.sh
+++ shell.sh
@@ -5,7 +5,7 @@ set -e -o pipefail
 if [ -n "$1" ]; then
     ESCAPED_ARGS=$(printf "%q " "$@")
     echo "$ESCAPED_ARGS"
-    docker-compose exec app-web su -l app -c "$ESCAPED_ARGS"
+    docker-compose exec shell su -l app -c "$ESCAPED_ARGS"
 else
-    docker-compose exec app-web su -l app
+    docker-compose exec shell su -l app
 fi
